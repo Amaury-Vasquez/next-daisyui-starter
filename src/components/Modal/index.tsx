acc2497d0d6eb8f2c25edcb98b45304ac2f4379a
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-07-20 15:19:33 -0600

    feat: create alert component

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -2,19 +2,11 @@ import clsx from "clsx";
 import { forwardRef, HTMLProps } from "react";
 import { AiOutlineClose } from "react-icons/ai";
 import Button from "../Button";
+import { POSITIONS } from "../../styles";
 
 export const MODAL_POSITIONS = {
   center: "modal-center",
-  right: "right-0",
-  left: "left-0",
-  top: "top-0",
-  "top-right": "top-0 right-0 rounded-tr-none rounded-br-none rounded-tl-none",
-  "top-left": "top-0 left-0 rounded-tl-none rounded-bl-none rounded-tr-none",
-  bottom: "bottom-0",
-  "bottom-right":
-    "bottom-0 right-0 rounded-tr-none rounded-br-none rounded-bl-none",
-  "bottom-left":
-    "bottom-0 left-0 rounded-tl-none rounded-bl-none rounded-br-none",
+  ...POSITIONS,
 };
 
 export interface ModalProps extends HTMLProps<HTMLDialogElement> {
--- src/styles/variants.ts
+++ src/styles/variants.ts
@@ -50,3 +50,21 @@ export const TEXT_COLORS = {
   base: "text-base-content",
   default: "",
 };
+
+export const COMMON_POSITIONS = {
+  "top-right": "top-0 right-0 rounded-tr-none rounded-br-none rounded-tl-none",
+  "top-left": "top-0 left-0 rounded-tl-none rounded-bl-none rounded-tr-none",
+
+  "bottom-right":
+    "bottom-0 right-0 rounded-tr-none rounded-br-none rounded-bl-none",
+  "bottom-left":
+    "bottom-0 left-0 rounded-tl-none rounded-bl-none rounded-br-none",
+};
+
+export const POSITIONS = {
+  right: "right-0",
+  left: "left-0",
+  bottom: "bottom-0",
+  top: "top-0",
+  ...COMMON_POSITIONS,
+};
