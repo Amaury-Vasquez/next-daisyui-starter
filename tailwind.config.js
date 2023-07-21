55b3cfb17bc25636b27b65cd52a7441bf2f10d98
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-07-20 22:01:23 -0600

    feat: move components to ui folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/styles/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/ui/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   daisyui: {
     themes: [
